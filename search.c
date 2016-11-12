1b50a289e57cce9fc463a175ccf8939b333f5d2b
matzeni <matzeni07@gmail.com>
2016-11-12 16:26:45 +0100

    add search function (required for insert)

---
+++ search.c
@@ -0,0 +1,41 @@
+/*
+ * search.c
+ *
+ *  Created on: 11.11.2016
+ *      Author: Matthias Nickel
+ */
+
+#include "btree.h"
+#include "search.h"
+
+short findElement(struct node* node, stdelement e){
+	short i, result = -1;
+	printf("\nSuche: %d", e);
+	node_pointer newNode;
+	do{
+		newNode = NULL;
+		for(i = 0; i < node->number_of_elements; i++){
+			//check if wanted value is in current node
+			if(node->elements[i] == e){
+				//set index of element
+				result = i;
+				break;
+			//if value of node element is larger than e set new node
+			} else if(node->elements[i] > e){
+				newNode = node->children[i];
+				break;
+			}
+		}
+		//case e is larger than all elements in current node
+		if(newNode == NULL && result == -1){
+			newNode = node->children[i];
+		}
+		//set current node to child node
+		if(newNode != NULL && result == -1){
+			node = newNode;
+		}
+	//stop loop if element was found or current node has no child node
+	}while(newNode != NULL);
+	//return index of element in current select node or -1 if element could not be found
+	return result;
+}
---
+++ search.h
@@ -0,0 +1,13 @@
+/*
+ * search.h
+ *
+ *  Created on: 11.11.2016
+ *      Author: Matthias Nickel
+ */
+
+#ifndef SEARCH_H_
+#define SEARCH_H_
+
+short findElement(struct node* node, stdelement e);
+
+#endif /* SEARCH_H_ */
