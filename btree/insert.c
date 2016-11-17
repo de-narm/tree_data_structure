06399d0ca5b8d5ccded7c2e71de8c6c692fafbbc
matzeni <matzeni07@gmail.com>
2016-11-17 08:42:15 +0100

    add comments and now use calloc instead of malloc

--- btree/insert.c
+++ btree/insert.c
@@ -20,7 +20,7 @@ short insert(stdelement e){
 		}
 	//if node is empty create new root with e as first element
 	}else{
-		root = malloc(sizeof(struct node));
+		root = calloc(1, sizeof(struct node));
 		root->elements[0] = e;
 		root->number_of_elements = 1;
 	}
@@ -38,7 +38,12 @@ struct node* insert_into_node(struct node* node, stdelement e){
 	struct node* newNode = NULL;
 	//case: node already has maximum number of elements
 	if(node->number_of_elements >= MAXNODE){
+		//element to 'expand' the array of elements
 		stdelement extra;
+		/*
+		 * insert new element to correct position in array
+		 * or if e is largest element, set to extra
+		 */
 		if(index == MAXNODE){
 			extra = e;
 		} else{
@@ -49,31 +54,38 @@ struct node* insert_into_node(struct node* node, stdelement e){
 			node->elements[index] = e;
 		}
 
+		//get middle element
 		stdelement middle = node->elements[ORDER];
-		newNode = malloc(sizeof(struct node));
+		newNode = calloc(1, sizeof(struct node));
+		//position of first element of right half
 		short right = ORDER + 1;
+		//set position of first child node of right half
 		short ci = (index >= ORDER) ? right : ORDER;
 
 		for(i = 0; i < ORDER; i++){
 			newNode->elements[i] = node->elements[right + i];
 			newNode->children[i] = node->children[ci + i];
+			//remove values of right half
 			node->elements[right + i] = 0;
 			node->children[ci + i] = NULL;
+			//change parent of children of newNode
 			if(newNode->children[i] != NULL){
 				newNode->children[i]->parent = newNode;
 			}
 		}
 
+		//do also the same to the last child node
 		newNode->children[i] = node->children[MAXNODE];
 		node->children[MAXNODE] = NULL;
 		if(newNode->children[i] != NULL){
 			newNode->children[i]->parent = newNode;
 		}
+		//set value of largest element to newNode
 		newNode->elements[ORDER - 1] = extra;
-
+		//set new size of split node
 		node->number_of_elements = ORDER;
 		newNode->number_of_elements = ORDER;
-
+		//if split node is root node create new root with middle element
 		if(node == root){
 			root = NULL;
 			insert(middle);
@@ -82,30 +94,51 @@ struct node* insert_into_node(struct node* node, stdelement e){
 			node->parent = root;
 			newNode->parent = root;
 		}else{
+			//give middle value to node above
 			struct node* newParent = insert_into_node(node->parent, middle);
 			i = getIndex(node->parent, middle);
+			//check if node above was also split
 			if(newParent == NULL){
+				/*
+				 * if not, set parent of newNode to parent of node
+				 * and add newNode as child
+				 */
 				newNode->parent = node->parent;
-
 				moveChildren(node->parent, i);
-				node->parent->children[i] = node;
 				node->parent->children[i+1] = newNode;
  			} else{
+ 				/*
+ 				 * case:
+ 				 * node above was also split and middle element is not
+ 				 * in parent of node
+ 				 */
  				if(i == -1){
+ 					//check if middle element is in NewParent
  					i = getIndex(newParent, middle);
  					newNode->parent = newParent;
-
  					if(i == -1){
+ 						/**
+ 						 * case:
+ 						 * middle was also middle element of node above
+ 						 */
  						moveChildren(newParent, 0);
- 						node->parent->children[ORDER] = node;
  						newParent->children[0] = newNode;
  					} else{
+ 						/*
+ 						 * case:
+ 						 * middle node is in newParent
+ 						 */
  						node->parent = newParent;
 
  						moveChildren(newParent, i);
  						newParent->children[i+1] = newNode;
  					}
  				}else{
+ 					/**
+ 					 * case:
+ 					 * node above got split and middle element got inserted
+ 					 * in left side
+ 					 */
  					newNode->parent = node->parent;
 
  					moveChildren(node->parent, i);
@@ -127,9 +160,8 @@ struct node* insert_into_node(struct node* node, stdelement e){
 }
 
 
-
 short getIndex(struct node* node, stdelement e){
-	unsigned short i, result = -1;
+	short i, result = -1;
 	for(i = 0; i < node->number_of_elements; i++){
 		if(node->elements[i] == e){
 			result = i;
